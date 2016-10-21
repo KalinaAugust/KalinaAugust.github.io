3ec7caea9aa2213a509bab931e352d87e8e7e83d
KalinaAugust <kalininaugust@gmail.com>
2016-10-21 17:11:54 +0300

    v1.0

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -35,7 +35,7 @@ $(document).ready(function() {
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
 	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
+		$.scrollTo($(".fck"), 800, {
 			offset: -90
 		});
 	});
