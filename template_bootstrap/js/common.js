edd8b75857a4a476c4669a1f0b954f572ddf81c4
KalinaAugust <kalininaugust@gmail.com>
2016-11-10 20:09:55 +0300

    123

--- template_bootstrap/js/common.js
+++ template_bootstrap/js/common.js
@@ -14,12 +14,6 @@ $(document).ready(function() {
 	});
 
 
-	//Таймер обратного отсчета
-	//Документация: http://keith-wood.name/countdown.html
-	//<div class="countdown" date-time="2015-01-07"></div>
-	var austDay = new Date($(".countdown").attr("date-time"));
-	$(".countdown").countdown({until: austDay, format: 'yowdHMS'});
-
 	//Попап менеджер FancyBox
 	//Документация: http://fancybox.net/howto
 	//<a class="fancybox"><img src="image.jpg" /></a>
