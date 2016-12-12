e6b677d679361cac1cc2fa8cd5a9ec37ed1dc854
KalinaAugust <kalininaugust@gmail.com>
2016-12-12 20:43:34 +0300

    1
    1

---
+++ Sailex/mail_pop.php
@@ -0,0 +1,10 @@
+<?php
+
+$recepient = "youmail@ya.ru";
+$sitename = "Sailex.by";
+
+$phone = trim($_GET["phone"]);
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Телефон: $phone";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
