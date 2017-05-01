05e7c3be79d309d038ae8358ded548c0c2151b0f
Kalinin Dzianis <kalininaugust@gmail.com>
2017-05-01 18:09:09 +0300

    Bytegain

---
+++ Bytegain/js/common.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+
+	//Документация: http://api.jquery.com/jquery.ajax/
+	$("form").submit(function() {
+		$.ajax({
+			type: "GET",
+			url: "mail.php",
+			data: $("form").serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				$.fancybox.close();
+			}, 1000);
+		});
+		return false;
+	});
+
+});
