b4c34d254c2fc0949cde5331135883b5aeaad1bc
Kalinin Dzianis <kalininaugust@gmail.com>
2017-07-11 19:39:07 +0300

    123

--- dota-git/js/common.js
+++ dota-git/js/common.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
 
 
-
-
-
 	//Плавный скролл до блока .div по клику на .scroll
 	//Документация: https://github.com/flesler/jquery.scrollTo
-	$("a.scroll").click(function() {
-		$.scrollTo($(".div"), 800, {
-			offset: -90
+	$(".turnir").click(function() {
+		$.scrollTo($(".third-section"), 800, {
+			offset: 0
 		});
 	});
 
