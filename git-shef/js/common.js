13385016b8904529c0e4004e9c158e4b228307a9
Kalinin Dzianis <kalininaugust@gmail.com>
2017-08-06 14:53:11 +0300

    123

--- git-shef/js/common.js
+++ git-shef/js/common.js
@@ -1,18 +1,40 @@
 $(document).ready(function () {
 
-    $('.main-page .first-slider').slick({
+    $('.main-page .billboard-slider').slick({
         infinite: true,
         autoplay: true,
         arrows: false,
-        speed: 700,
+        speed: 1000,
         fade: true,
-        autoplaySpeed: 2500,
+        autoplaySpeed: 3000,
         cssEase: 'linear'
     });
 
+    $('.main-page .recommendation-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        centerMode: true,
+        nextArrow: '<div class="main-page__next-arrow"></div>',
+        prevArrow: '<div class="main-page__prev-arrow"></div>'
+    });
 
-    // nextArrow: '<div class="main-page__next-arrow"></div>',
-        // prevArrow: '<div class="main-page__prev-arrow"></div>'
+    $('.main-page .cafe-slider').slick({
+        slidesToShow: 5,
+        centerMode: true,
+        centerPadding: '80px',
+        nextArrow: '<div class="main-page__next-arrow"></div>',
+        prevArrow: '<div class="main-page__prev-arrow"></div>'
+    });
+
+    $('.main-page .actions-slider').slick({
+        slidesToShow: 1,
+        infinite: true,
+        nextArrow: '<div class="main-page__next-arrow"></div>',
+        prevArrow: '<div class="main-page__prev-arrow"></div>'
+    });
+
+
 
     // // pop up activator
     // $('.button__pop-up').magnificPopup({
