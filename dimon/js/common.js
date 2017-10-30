b0a294f6378eb4086f28d8df976aa3d06cf775db
Kalina <kalininaugust@gmail.com>
2017-10-30 12:20:50 +0300

    123

--- dimon/js/common.js
+++ dimon/js/common.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+    var header = $('header.header');
+    var menu = $('.header__nav');
+
+    $("body").click(function (event) {
+        var target = $( event.target );
+
+        if ( target.is('.burger') ) {
+
+            menu.toggleClass('menu-open');
+
+        } else if ( target.closest(header).hasClass('header')  ) {
+
+            return;
+
+        } else {
+            menu.removeClass('menu-open');
+        }
+    });
+
 });
