6cfab6b608db889bfa20fc520b0b8d72a00ce616
kalininds <denis.kalinin@oxagile.com>
2019-12-02 17:46:02 +0300

    1

--- scrollToNoLIbs.js
+++
@@ -1,7 +0,0 @@
-    // scrollTo clean jquery
-
-    $(".to-top").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#main-form").offset().top -100
-        }, 800);
-    });
