a096cc8fde3295f534852794ebf5ad042ff2d173
Kalinin Dzianis <kalininaugust@gmail.com>
2023-10-04 17:17:39 +0200

    save push

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+$(document).ready(function (){
+    $("#click").click(function (){
+        const to = $(this).attr('data-scroll');
+        $('html, body').animate({
+            scrollTop: $("#" + to).offset().top
+        }, 1000);
+    });
+});
