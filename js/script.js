395a6090724edecd75308f0cfa9b720c78511e86
Kalinin Dzianis <kalininaugust@gmail.com>
2023-10-07 03:04:17 +0200

    save push

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(document).ready(function (){
-    $("#click").click(function (){
+    $(".click-to-scroll").click(function (){
         const to = $(this).attr('data-scroll');
         $('html, body').animate({
             scrollTop: $("#" + to).offset().top - 100
@@ -20,15 +20,93 @@ $(document).ready(function (){
     $('.gallery-container').magnificPopup({
         type:'image',
         delegate: 'a',
-        gallery: {enabled:true},
+        gallery: { enabled:true },
     });
 
+    // form popup
+    $('.open-popup-link').magnificPopup({
+        type:'inline',
+        closeBtnInside: true,
+        midClick: true
+    });
+
+    function formsReset() {
+        $("#name").val('');
+        $("#email").val('');
+        $("#phone").val('');
+        $("#message").val('');
+
+        $("#cv-name").val('');
+        $("#cv-email").val('');
+        $("#cv-phone").val('');
+        $("#cv-message").val('');
+        $("#cv-profession").val('');
+    }
+
     // send email from contact form
-    $("#submit-contact-form").click(function () {
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var phone = $("#phone").val();
-        var message = $("#message").val();
+    $(".submit-form-button").click(function () {
+        var formType = $(this).attr('data-form-type');
+        var hasErrors = false;
+
+        var name = '';
+        var email = '';
+        var phone = '';
+        var message = '';
+        var profession = '';
+        var form = '';
+
+        if (formType === 'main-form') {
+            name = $("#name").val().trim();
+            email = $("#email").val().trim();
+            phone = $("#phone").val().trim();
+            message = $("#message").val().trim();
+            form = 'Main Form';
+
+            if (name.length < 2) {
+                $("#name").addClass('invalid');
+                hasErrors = true;
+            } else {
+                $("#name").removeClass('invalid');
+            }
+
+            if (email.length < 2) {
+                $("#email").addClass('invalid');
+                hasErrors = true;
+            } else {
+                $("#email").removeClass('invalid');
+            }
+        } else if (formType === 'cv-form') {
+            name = $("#cv-name").val().trim();
+            email = $("#cv-email").val().trim();
+            phone = $("#cv-phone").val().trim();
+            message = $("#cv-message").val().trim();
+            profession = $("#cv-profession").val().trim();
+            form = 'CV Form';
+
+            if (name.length < 2) {
+                $("#cv-name").addClass('invalid');
+                hasErrors = true;
+            } else {
+                $("#cv-name").removeClass('invalid');
+            }
+
+            if (email.length < 2) {
+                $("#cv-email").addClass('invalid');
+                hasErrors = true;
+            } else {
+                $("#cv-email").removeClass('invalid');
+            }
+        } else {
+
+        }
+
+        if (hasErrors) return;
+        formsReset();
+
+        $.notify(
+            "Już wysłane",
+            { position: "bottom", className: "success", }
+        );
 
         $.ajax({
             url: "mail.php",
@@ -38,9 +116,11 @@ $(document).ready(function (){
                 email: email,
                 phone: phone,
                 message: message,
+                profession: profession,
+                form: form,
             },
-            success: function (response) {
-                $("#response").html(response);
+            success: function () {
+
             }
         });
     });
--- mail.php
+++ mail.php
@@ -1,13 +1,17 @@
 <?php
     $name = $_POST["name"];
+    $formName = $_POST["form-name"];
     $email = $_POST["email"];
+    $phone = $_POST["phone"];
     $message = $_POST["message"];
 
     $to = "recipient@example.com";
     $subject = "Contact Form Submission from M&A";
-
     $from = "test@hostinger-tutorials.com";
     $headers = "From:" . $from;
-    mail($to, $subject, $message, $headers);
+
+    $formatedMessage = "Форма: $formName \nИмя: $name \nТелефон: $phone \nПочта: $email \nТекст: $message";
+
+    mail($to, $subject, $formatedMessage, $headers);
     echo "The email message was sent.";
 ?>
